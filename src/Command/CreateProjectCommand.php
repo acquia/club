409a2ec04151b6e795337608a4427ffed041e46a
Matthew Grasmick <matthew.grasmick@acquia.com>
2016-10-04 11:32:26 -0400

    Breaking up large execute() method.

--- src/Command/CreateProjectCommand.php
+++ src/Command/CreateProjectCommand.php
@@ -53,30 +53,8 @@ class CreateProjectCommand extends CommandBase
         if ($recipe_filename) {
             $answers = $this->loadRecipe($recipe_filename);
             $this->checkDestinationDir($answers['machine_name']);
-        }
-        else {
-            $this->output->writeln("<info>Let's start by entering some information about your project.</info>");
-
-            $question = new Question('<question>Project title (human readable):</question> ');
-            $this->requireQuestion($question);
-            $answers['human_name'] = $this->questionHelper->ask($input, $output, $question);
-
-            $default_machine_name = self::convertStringToMachineSafe($answers['human_name']);
-            $question = new Question("<question>Project machine name:</question> <info>[$default_machine_name]</info> ", $default_machine_name);
-            $answers['machine_name'] = $this->questionHelper->ask($input, $output, $question);
-
-            $this->checkDestinationDir($answers['machine_name']);
-
-            $default_prefix = self::convertStringToPrefix($answers['human_name']);
-            $question = new Question("<question>Project prefix:</question> <info>[$default_prefix]</info>", $default_prefix);
-            $answers['prefix'] = $this->questionHelper->ask($input, $output, $question);
-
-            $this->output->writeln("<info>Great. Now let's make some choices about how your project will be set up.</info>");
-            $question = new ConfirmationQuestion('<question>Do you want to create a VM?</question> <info>[yes]</info> ', true);
-            $answers['vm'] = $this->questionHelper->ask($input, $output, $question);
-
-            // $question = new ConfirmationQuestion('<question>Do you want to create an Acquia Cloud free tier site for this project?</question> ', false);
-            // $create_acf_site = $helper->ask($input, $output, $question);
+        } else {
+            $answers = $this->askForAnswers();
         }
 
         $this->output->writeln("<comment>You have entered the following values:</comment>");
@@ -85,39 +63,8 @@ class CreateProjectCommand extends CommandBase
         $create = $this->questionHelper->ask($input, $output, $question);
 
         if ($create) {
-            $this->output->writeln("<info>Awesome. Let's create your project. This could take a while...");
-
-            $this->executeCommands([
-            "composer create-project acquia/blt-project:~8 {$answers['machine_name']} --no-interaction",
-            ]);
-
-            $cwd = getcwd() . '/' . $answers['machine_name'];
-            $config_file = $cwd . '/project.yml';
-            $config = Yaml::parse(file_get_contents($config_file));
-            $config['project']['prefix'] = $answers['prefix'];
-            $config['project']['machine_name'] = $answers['machine_name'];
-            $config['project']['human_name'] = $answers['human_name'];
-            // Hostname cannot contain underscores.
-            $machine_name_safe = str_replace('_', '-', $answers['machine_name']);
-            $config['project']['local']['hostname'] = str_replace('${project.machine_name}', $machine_name_safe, $config['project']['local']['hostname']);
-            $this->fs->dumpFile($config_file, Yaml::dump($config));
-
-            if ($answers['vm']) {
-                $this->executeCommands([
-                "./vendor/bin/blt vm",
-                "./vendor/bin/blt local:setup",
-                "./vendor/bin/drush @{$answers['machine_name']}.local uli",
-                ], $cwd);
-                $this->output->writeln();
-            } else {
-                $this->output->writeln();
-            }
+            $this->createProject($answers);
         }
-
-        // @todo Push to Acquia Cloud.
-        // @todo Trigger pipelines build.
-        // @todo Deploy branch and install.
-        // @todo drush uli remote.
     }
 
   /**
@@ -140,9 +87,9 @@ class CreateProjectCommand extends CommandBase
      *
      * @return array
      */
-    protected function loadRecipe($filename) {
-        if (!file_exists($filename))
-        {
+    protected function loadRecipe($filename)
+    {
+        if (!file_exists($filename)) {
             throw new FileNotFoundException($filename);
         }
 
@@ -158,6 +105,70 @@ class CreateProjectCommand extends CommandBase
         );
 
         return $processed_configuration;
+    }
+
+    protected function askForAnswers()
+    {
+        $this->output->writeln("<info>Let's start by entering some information about your project.</info>");
+
+        $question = new Question('<question>Project title (human readable):</question> ');
+        $this->requireQuestion($question);
+        $answers['human_name'] = $this->questionHelper->ask($this->input, $this->output, $question);
+
+        $default_machine_name = self::convertStringToMachineSafe($answers['human_name']);
+        $question = new Question("<question>Project machine name:</question> <info>[$default_machine_name]</info> ", $default_machine_name);
+        $answers['machine_name'] = $this->questionHelper->ask($this->input, $this->output, $question);
+
+        $this->checkDestinationDir($answers['machine_name']);
+
+        $default_prefix = self::convertStringToPrefix($answers['human_name']);
+        $question = new Question("<question>Project prefix:</question> <info>[$default_prefix]</info>", $default_prefix);
+        $answers['prefix'] = $this->questionHelper->ask($this->input, $this->output, $question);
+
+        $this->output->writeln("<info>Great. Now let's make some choices about how your project will be set up.</info>");
+        $question = new ConfirmationQuestion('<question>Do you want to create a VM?</question> <info>[yes]</info> ', true);
+        $answers['vm'] = $this->questionHelper->ask($this->input, $this->output, $question);
+
+        // $question = new ConfirmationQuestion('<question>Do you want to create an Acquia Cloud free tier site for this project?</question> ', false);
+        // $create_acf_site = $helper->ask($input, $output, $question);
+
+        return $answers;
+    }
+
+    protected function createProject($answers)
+    {
+        $this->output->writeln("<info>Awesome. Let's create your project. This could take a while...");
+
+        $this->executeCommands([
+            "composer create-project acquia/blt-project:~8 {$answers['machine_name']} --no-interaction",
+        ]);
+
+        $cwd = getcwd() . '/' . $answers['machine_name'];
+        $config_file = $cwd . '/project.yml';
+        $config = Yaml::parse(file_get_contents($config_file));
+        $config['project']['prefix'] = $answers['prefix'];
+        $config['project']['machine_name'] = $answers['machine_name'];
+        $config['project']['human_name'] = $answers['human_name'];
+        // Hostname cannot contain underscores.
+        $machine_name_safe = str_replace('_', '-', $answers['machine_name']);
+        $config['project']['local']['hostname'] = str_replace('${project.machine_name}', $machine_name_safe, $config['project']['local']['hostname']);
+        $this->fs->dumpFile($config_file, Yaml::dump($config));
+
+        if ($answers['vm']) {
+            $this->executeCommands([
+                "./vendor/bin/blt vm",
+                "./vendor/bin/blt local:setup",
+                "./vendor/bin/drush @{$answers['machine_name']}.local uli",
+            ], $cwd);
+            $this->output->writeln();
+        } else {
+            $this->output->writeln();
+        }
+
+        // @todo Push to Acquia Cloud.
+        // @todo Trigger pipelines build.
+        // @todo Deploy branch and install.
+        // @todo drush uli remote.
     }
 
   /**
